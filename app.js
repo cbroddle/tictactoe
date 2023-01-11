aae6fc847ec4cd17729087c1875b0ca729107b79
Christen Broddle <christen@Christens-MBP.localdomain>
2023-01-11 11:08:21 -0800

    App JS in progress, gameboard functional on click

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 //store gameboard as an array inside of a gameboard object
-const gameChoices = ['X','O','X','O','X','O','X'];
-//Select part of grid
-const gridSection = document.getElementById('grid-top1');
+const gameChoices = [];
 
-/*const gameBoard = (choice, position) => {
-    const playGame = () => console.log('choice');
-    return { choice, position };
-};*/
+//Player chooses space on grid
+const grid = document.querySelectorAll('.grid-section');
+grid.forEach((grid) => {
+    grid.addEventListener("click",
+        () => {
+            //Display last choice in array
+            grid.innerText = `${gameChoices[gameChoices.length - 1]}`;
+        })
+})
 
-//display gameChoices on gameBoard
-gridSection.innerText = `${gameChoices[0]}`;
+//Function taking in player game choice
+const xChoice = document.querySelector('button.x');
+const oChoice = document.querySelector('button.o');
+xChoice.addEventListener("click",
+    function () {
+        if (gameChoices.length < 9){
+        gameChoices.push('X');
+        console.log('X');
+        } else if(gameChoices.length === 9){
+            alert("Gameover");
+            //insert scoreGame or declare winner function
+        } else {
+            alert("Error");
+        }
+    })
+oChoice.addEventListener("click",
+    function () {
+        gameChoices.push('O');
+        console.log('O');
+    })
+
+//Function for random computer play?
 
 
 
