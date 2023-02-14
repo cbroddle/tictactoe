3a2affd01be8d4b0dcca06d4d48e0d8cf393d5be
Christen Broddle <christen@Christens-MBP.localdomain>
2023-02-13 21:42:14 -0800

    UI change, playerChoice in progress

--- app.js
+++ app.js
@@ -1,36 +1,75 @@
 //store gameboard as an array inside of a gameboard object
 const gameChoices = [];
+const gridPosition = [];
 
 //Player chooses space on grid
 const grid = document.querySelectorAll('.grid-section');
+const selectButtons = document.querySelector('.player-choice');
 grid.forEach((grid) => {
     grid.addEventListener("click",
         () => {
-            //Display last choice in array
-            grid.innerText = `${gameChoices[gameChoices.length - 1]}`;
+            //display choices X or O
+            selectButtons.classList.toggle("show-player-choice");
+            //collect grid position
+            gridPosition.push(grid.id);
+            console.log(gridPosition);
+            //Needs to show in space of grid selected, not just position 0
+            
+            //Push Choice to gameChoices array
+            //display last value of gameChoices?
+            //grid.innerText = `${gameChoices[gameChoices.length - 1]}`;
         })
 })
 
+//All combinations of grid position to win with 3 of a kind in a row
+//Moves can be in any order(ex: 0,1,2 can be 1,0,2...)
+let winnerMatrix = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 //Function taking in player game choice
 const xChoice = document.querySelector('button.x');
-const oChoice = document.querySelector('button.o');
 xChoice.addEventListener("click",
     function () {
-        if (gameChoices.length < 9){
         gameChoices.push('X');
-        console.log('X');
-        } else if(gameChoices.length === 9){
-            alert("Gameover");
-            //insert scoreGame or declare winner function
-        } else {
-            alert("Error");
-        }
+        //Display X
+        grid.innerText = 'X';
     })
+
+
+
+const oChoice = document.querySelector('button.o');
+
 oChoice.addEventListener("click",
     function () {
         gameChoices.push('O');
-        console.log('O');
+        //Display O
+        grid.innerText = 'O';
     })
+
+
+//Function to check for winner
+function checkWinner() {
+    //Retrieve and check 
+    //3 X's in a row, gameChoices contains X, X, X, 
+    let checkX = gameChoices.filter(x => x.includes("X"));
+    console.log(checkX);
+    if (checkX.length === 3) {
+        console.log("X wins");
+        //Bug: 3 X'x present but not in a row
+        //Check matrix here?
+    }
+
+    //3 O's in a row
+    //Neither 3 X's or 3 O's in a row and all spaces full(array has 9 entries)
+}
 
 //Function for random computer play?
 
